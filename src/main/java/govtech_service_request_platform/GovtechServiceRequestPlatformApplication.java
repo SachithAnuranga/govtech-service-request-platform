39814f6bbaad5e0c5fd1d0473f6e6108da31ce38
sachith_anuranga <sachith635@gmail.com>
2026-06-19 10:59:50 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/govtech_service_request_platform/GovtechServiceRequestPlatformApplication.java
@@ -0,0 +1,13 @@
+package govtech_service_request_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GovtechServiceRequestPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GovtechServiceRequestPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/govtech_service_request_platform/GovtechServiceRequestPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package govtech_service_request_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GovtechServiceRequestPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
