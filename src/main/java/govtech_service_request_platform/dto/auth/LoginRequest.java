765fbaaebee98d98cea99137de753a1179626418
sachith_anuranga <sachith635@gmail.com>
2026-06-19 23:07:47 +0530

    Login Request and Response DTO

---
+++ src/main/java/govtech_service_request_platform/dto/auth/LoginRequest.java
@@ -0,0 +1,13 @@
+package govtech_service_request_platform.dto.auth;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginRequest {
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+}
